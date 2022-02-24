20f8d85bb4834b33edbe0a3794ac1156d6b30bbf
Tomek Regulski <tomek.regulski@gmail.com>
2022-02-24 12:15:51 -0500

    define pub fn's

--- programs/solana-anchor-escrow/src/lib.rs
+++ programs/solana-anchor-escrow/src/lib.rs
@@ -7,23 +7,100 @@ declare_id!("BgBM8rneuxRfido2bBwapiJYPVddQszTJhcjJkaHQV83");
 #[program]
 pub mod solana_anchor_escrow {
     use super::*;
+    const ESCROW_PDA_SEED: &[u8] = b"escrow";
     // Initialize can be considered as a wrapper for instructions. This wrapper is enhanced by Anchor via derived macro (#[derive(account)]). 
+    
+    // In initialize, what happens is that the input accounts are assigned to EscrowAccount fields one by one. Then, a program derived address, or PDA, is derived to be going to become new authority of initializer_deposit_token_account.
     pub fn initialize(
         ctx: Context<Initialize>, 
         _vault_account_bump: u8, 
         initializer_amount: u64, 
         taker_amount: u64
     ) -> ProgramResult {
+        ctx.accounts.escrow_account.initializer_key = *ctx.accounts.initializer.key;
+        ctx.accounts
+            .escrow_account
+            .initializer_deposit_token_account = *ctx
+            .accounts
+            .initializer_deposit_token_account
+            .to_account_info()
+            .key;
+        ctx.accounts
+            .escrow_account
+            .initializer_receive_token_account = *ctx
+            .accounts
+            .initializer_receive_token_account
+            .to_account_info()
+            .key;
+        ctx.accounts.escrow_account.initializer_amount = initializer_amount;
+        ctx.accounts.escrow_account.taker_amount = taker_amount;
+
+        let (vault_authority, _vault_authority_bump) =
+            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        token::set_authority(
+            ctx.accounts.into_set_authority_context(),
+            AuthorityType::AccountOwner,
+            Some(vault_authority),
+        )?;
+
+        token::transfer(
+            ctx.accounts.into_transfer_to_pda_context(),
+            ctx.accounts.escrow_account.initializer_amount,
+        )?;
+
         Ok(())
     }
 
+    // In cancel, it just simply reset the authority from PDA back to the initializer.
     pub fn cancel(ctx: Context<Cancel>) -> ProgramResult {
-        // TODO
+        let (_vault_authority, vault_authority_bump) =
+            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
+
+        token::transfer(
+            ctx.accounts
+                .into_transfer_to_initializer_context()
+                .with_signer(&[&authority_seeds[..]]),
+            ctx.accounts.escrow_account.initializer_amount,
+        )?;
+
+        token::close_account(
+            ctx.accounts
+                .into_close_context()
+                .with_signer(&[&authority_seeds[..]]),
+        )?;
+
         Ok(())
     }
 
+    // In exchange, 3 things happen:
+    //      First, token A gets transfered from pda_deposit_token_account to taker_receive_token_account.
+    //      Next, token B gets transfered from taker_deposit_token_account to initializer_receive_token_account.
+    //      Finally, the authority of pda_deposit_token_account gets set back to the initializer.
+    //
     pub fn exchange(ctx: Context<Exchange>) -> ProgramResult {
-        // TODO
+        let (_vault_authority, vault_authority_bump) =
+            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
+
+        token::transfer(
+            ctx.accounts.into_transfer_to_initializer_context(),
+            ctx.accounts.escrow_account.taker_amount,
+        )?;
+
+        token::transfer(
+            ctx.accounts
+                .into_transfer_to_taker_context()
+                .with_signer(&[&authority_seeds[..]]),
+            ctx.accounts.escrow_account.initializer_amount,
+        )?;
+
+        token::close_account(
+            ctx.accounts
+                .into_close_context()
+                .with_signer(&[&authority_seeds[..]]),
+        )?;
+
         Ok(())
     }
 }
